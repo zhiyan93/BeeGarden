535416b49c4944564a0d0a829821eca1414de5ca
zhiyan liu <42515218+zhiyan93@users.noreply.github.com>
2020-04-06 03:16:32 +1000

    day 2 add observe tableview controller

---
+++ BeeGarden/AddObserveVC.swift
@@ -0,0 +1,30 @@
+//
+//  AddObserveVC.swift
+//  BeeGarden
+//
+//  Created by steven liu on 5/4/20.
+//  Copyright © 2020 steven liu. All rights reserved.
+//
+
+import UIKit
+
+class AddObserveVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BeeGarden/GalleryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GalleryTableViewCell.swift
+//  BeeGarden
+//
+//  Created by steven liu on 5/4/20.
+//  Copyright © 2020 steven liu. All rights reserved.
+//
+
+import UIKit
+
+class GalleryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
