f7bac2f93fd5926f26308e2d5ded3f0d450512f1
zhiyan liu <42515218+zhiyan93@users.noreply.github.com>
2020-04-16 03:59:49 +1000

    rain forecast and history

---
+++ BeeGarden/LaunchScreenVC.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchScreenVC.swift
+//  BeeGarden
+//
+//  Created by steven liu on 14/4/20.
+//  Copyright © 2020 steven liu. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BeeGarden/PlantVC.swift
@@ -0,0 +1,30 @@
+//
+//  PlantVC.swift
+//  BeeGarden
+//
+//  Created by steven liu on 15/4/20.
+//  Copyright © 2020 steven liu. All rights reserved.
+//
+
+import UIKit
+
+class PlantVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
